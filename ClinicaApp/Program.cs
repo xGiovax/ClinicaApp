7eb915bdd8ae1cb73ebc0b5f51920685b791c280
MINEDUCYT <MINEDUCYT@DESKTOP-VDTUKI5>
2025-09-21 18:16:29 -0600

    Modificacion de la direccion de los botones

--- ClinicaApp/Program.cs
+++ ClinicaApp/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Pacientes}/{action=Index}/{id?}");
 
 app.Run();
